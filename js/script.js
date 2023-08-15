62063198b2cf5fba3c609f24f49615e2974775a4
MaximilianoD98 <maximilianodurandg@gmail.com>
2023-08-15 20:15:12 -0300

    Actualizacion final

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 //CONSTANTES
 
-const autopartes = {
+const apiUrl = 'https://64d94520e947d30a260a080b.mockapi.io/mdmotors/api/v1/MDMotors';
 
-  autopartes : [
-    { numero: "1", nombre: "KIT-CAMBIO",valor: 1000 , img: "./img/kitcambio.jpg", tipos: ["Piezas mecanicas"] },
-    { numero: "2", nombre: "KIT-MOTOR",valor: 5000 , img: "./img/kitmotor.jpg", tipos: ["Piezas mecanicas"] },
-    { numero: "3", nombre: "KIT-FRENOS",valor: 1400 , img: "./img/kitfrenos.jpg", tipos: ["Piezas mecanicas"] },
-    { numero: "4", nombre: "KIT-ESCAPE",valor: 1300 , img: "./img/kitescape.jpg", tipos: ["Piezas mecanicas"] },
-    { numero: "5", nombre: "KIT-METALES",valor: 3000 , img: "./img/kitmetales.jpg", tipos: ["Piezas mecanicas"] },
-    { numero: "6", nombre: "BODY-KIT",valor: 4500 , img: "./img/bodykit.jpg", tipos: ["Piezas esteticas"] },
-    { numero: "7", nombre: "KIT-LLANTAS",valor: 1200 , img: "./img/kitllantas.jpg", tipos: ["Piezas esteticas"] },
-    { numero: "8", nombre: "KIT-POLARIZADO",valor: 800 , img: "./img/kitpolarizado.jpg", tipos: ["Piezas esteticas"] },
-    { numero: "9", nombre: "KIT-CALCOMANIAS",valor: 500 , img: "./img/kitcalcomanias.jpg", tipos: ["Piezas esteticas"] },
-    { numero: "10", nombre: "KIT-LUZ-NEON",valor: 900 , img: "./img/kitneon.jpg", tipos: ["Piezas esteticas"] }
-    ],
-}
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const autopartes = {
+      autopartes: data.map(item => ({
+        numero: item.id,
+        nombre: item.nombre,
+        valor: item.valor,
+        img: item.imagen,
+        tipos: item.tipos
+      }))
+    };
 
-const cambioModo = document.getElementById("cambio_modo");
-const body = document.body;
+    console.log(autopartes); // Muestra el diccionario en la consola
+
+    const body = document.body;
 
 const botonTodasPiezas = document.getElementById("todasPiezas_id");
 const botonPiezasMec = document.getElementById("piezas_mec_id");
@@ -25,29 +30,44 @@ const botonPiezasEst = document.getElementById("piezas_est_id");
 const autoPartesContent = document.getElementById("autoPartesContent");
 const autoParteBoxConteiner = document.getElementById("autoParteBoxCont");
 const listaAgregados = [];
-const cajasOcupadas=[];
+const cajasOcupadas = [];
 
-// CAMBIO DE MODO
+// FUNCION PARA MOSTRAR TODOS LOS PRODUCTOS
 
-const modoActual = localStorage.getItem("modo");
-if (modoActual) {
-  body.classList.add(modoActual);
-}
+  botonTodasPiezas.addEventListener("click", function() {
+    const autoPartesContent = document.getElementById("autoPartesContent");
 
-cambioModo.addEventListener("click", () => {
-  if (body.classList.contains("dark-mode")) {
-    body.classList.remove("dark-mode");
-    localStorage.setItem("modo", "light-mode");
-  } else {
-    body.classList.add("dark-mode");
-    localStorage.setItem("modo", "dark-mode");
-  }
-});
+    autoPartesContent.innerHTML = '';
 
-//BOTONES
+    autopartes.autopartes.forEach(function(autoParte) {
+      mostrarAutopartes(autoParte, autoPartesContent);
+    });
+     function mostrarAutopartes(autoParte, container) {
+      const tipos = autoParte.tipos.join(', ');
+      
+      const autoPartesElement = document.createElement("div");
+      autoPartesElement.classList.add("autoParte");
+      autoPartesElement.innerHTML = `
 
+        <p><b>Número:</b> ${autoParte.numero}</p>
+        <p><b>Nombre:</b> ${autoParte.nombre}</p>
+        <p><b>Tipos:</b> ${tipos}</p>
+        <p><b>Valor:</b> ${autoParte.valor}</p>
+        <img class="img-productos" src=${autoParte.img}></img>
+        <button id="${autoParte.numero}">Ingresar</button>
+      `;
+        
+      
+      container.appendChild(autoPartesElement);
 
-// GUARDAR LA LISTA EN JSON
+      let agregador = activarBoton(autoParte.numero);
+        agregador.addEventListener("click",function(){
+          agregarLista(autoParte);
+        })
+      }
+    });
+
+    // GUARDAR LA LISTA EN JSON
 
 function guardarAutopartes() {
   localStorage.setItem("autoPartes_U", JSON.stringify(listaAgregados));
@@ -64,43 +84,6 @@ window.addEventListener("load", function() {
   }
 });
 
-
-// FUNCION PARA MOSTRAR TODOS LOS PRODUCTOS
-
-botonTodasPiezas.addEventListener("click", function() {
-  const autoPartesContent = document.getElementById("autoPartesContent");
-
-  autoPartesContent.innerHTML = '';
-
-  autopartes.autopartes.forEach(function(autoParte) {
-    mostrarAutopartes(autoParte, autoPartesContent);
-  });
-  function mostrarAutopartes(autoParte, container) {
-    const tipos = autoParte.tipos.join(', ');
-  
-    const autoPartesElement = document.createElement("div");
-    autoPartesElement.classList.add("autoParte");
-    autoPartesElement.innerHTML = `
-
-    <p><b>Número:</b> ${autoParte.numero}</p>
-    <p><b>Nombre:</b> ${autoParte.nombre}</p>
-    <p><b>Tipos:</b> ${tipos}</p>
-    <p><b>Valor:</b> ${autoParte.valor}</p>
-    <img class="img-productos" src=${autoParte.img}></img>
-    <button id="${autoParte.numero}">Ingresar</button>
-	`;
-    
-  
-    container.appendChild(autoPartesElement);
-
-    let agregador = activarBoton(autoParte.numero);
-    agregador.addEventListener("click",function(){
-      agregarLista(autoParte);
-    })
-  }
-});
-
-
 // FUNCION PARA MOSTRAR MEC
 
 botonPiezasMec.addEventListener("click", function() {
@@ -115,8 +98,8 @@ botonPiezasMec.addEventListener("click", function() {
     const tipos = autoParte.tipos.join(', ');
     if(tipos=="Piezas mecanicas"){
 
-    
-    
+      
+      
       const autoPartesElement = document.createElement("div");
       autoPartesElement.classList.add("autoParte");
       autoPartesElement.innerHTML = `
@@ -128,8 +111,8 @@ botonPiezasMec.addEventListener("click", function() {
       <img class="img-productos" src=${autoParte.img}></img>
       <button id="${autoParte.numero}">Ingresar</button>
     `;
+        
       
-    
       container.appendChild(autoPartesElement);
 
       let agregador = activarBoton(autoParte.numero);
@@ -154,8 +137,8 @@ botonPiezasEst.addEventListener("click", function() {
     const tipos = autoParte.tipos.join(', ');
     if(tipos=="Piezas esteticas"){
 
-    
-    
+      
+      
       const autoPartesElement = document.createElement("div");
       autoPartesElement.classList.add("autoParte");
       autoPartesElement.innerHTML = `
@@ -167,8 +150,8 @@ botonPiezasEst.addEventListener("click", function() {
       <img class="img-productos" src=${autoParte.img}></img>
       <button id="${autoParte.numero}">Ingresar</button>
     `;
+        
       
-    
       container.appendChild(autoPartesElement);
 
       let agregador = activarBoton(autoParte.numero);
@@ -178,6 +161,26 @@ botonPiezasEst.addEventListener("click", function() {
   }
 }
 });
+
+const cambioModo = document.getElementById("cambio_modo");
+
+// CAMBIO DE MODO
+
+const modoActual = localStorage.getItem("modo");
+if (modoActual) {
+  body.classList.add(modoActual);
+}
+
+cambioModo.addEventListener("click", () => {
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("modo", "light-mode");
+  } else {
+    body.classList.add("dark-mode");
+    localStorage.setItem("modo", "dark-mode");
+  }
+});
+
 // ACTIVADOR DE BOTON
 
 function activarBoton(id){
@@ -238,22 +241,12 @@ function mostrarAutopartesAgregados() {
 // FUNCIÓN PARA BOTON ELIMINAR
 
 function eliminarAutopartes(nombreAutoParte) {
-  for (x in listaAgregados){
-    if(listaAgregados[x].nombre==nombreAutoParte){
-    
-      listaAgregados.splice(x,1)
-    }
-
-  }
   const index = listaAgregados.findIndex(autoParte => autoParte.nombre === nombreAutoParte);
 
   if (index !== -1) {
-  
     listaAgregados.splice(index, 1);
+    guardarAutopartes();
   }
-  
-  guardarAutopartes();
-
 }
 
 
@@ -295,4 +288,37 @@ function mostrarAutopartes(autoParte) {
     autoPartesContent.innerHTML = '';
     autoPartesContent.appendChild(autoPartesElement);
   }
+}
+
+
+
+repetirAlerta();
+
+fetch})
+  .catch(error => {
+    console.error("Error en la solicitud:", error);
+  });
+
+  
+function mostrarAlerta() {
+    
+    Toastify({
+      text: "Miércoles y viernes, un 20% off.",
+      duration: 5000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "left", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #767583, #1C0DB4)",
+      },
+      onClick: function(){} // Callback after click
+    }).showToast();
+  }
+
+function repetirAlerta() {
+  mostrarAlerta();
+  setTimeout(repetirAlerta, 20000);
 }
